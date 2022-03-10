ce979ae9e1267f6b560c159a1cac0b4e7167904d
Muhammad Atif Yushri <atif@yushri.com>
2022-03-11 00:35:38 +0800

    Task 3 initialised. Still contain logic errors.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # Python 3.10.1
+from random import randint
 from tabulate import tabulate
 
-Days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+Days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 TicketType = ["One adult", "One child", "One senior", "Family ticket (2 adults/senior + 3 children)", "Groups of 6 (per person)"]
 TicketTypeOneDay = [20, 12, 16, 60, 15]
@@ -13,19 +14,19 @@ AttractionPrice = [2.5, 2, 5]
 def TicketPrices():
     # Reorganised price data into 2D arrays
     Data = []
-    for i in range(len(Days) - 1):
-        n = [TicketType[i], TicketTypeOneDay[i], TicketTypeTwoDays[i]]
+    for i in range(5):
+        n = [i + 1, TicketType[i], TicketTypeOneDay[i], TicketTypeTwoDays[i]]
         Data.append(n)
     
-    print("\n", tabulate(Data , headers = ["Ticket type", "One day", "Two days"]), "\n")
+    print("\n", tabulate(Data , headers = ["Input number", "Ticket type", "One day", "Two days"]), "\n")
 
     # Reorganised attraction price data into 2D arrays
     Data = []
-    for i in range(len(AttractionPrice) - 1):
-        n = [AttractionType[i], AttractionPrice[i]]
+    for i in range(len(AttractionType)):
+        n = [i + 1, AttractionType[i], AttractionPrice[i]]
         Data.append(n)
 
-    print("\n", tabulate(Data, headers = ["Extra attractions", "Cost per person"]), "\n")
+    print("\n", tabulate(Data, headers = ["Input number", "Extra attractions", "Cost per person"]), "\n")
     
     # Days reorgs
     Data = []
@@ -38,18 +39,97 @@ def TicketPrices():
 TicketPrices()
 
 def TicketBooking():
-    PurchaseTickets = []
-    while True:
-        PurchaseTickets.append(input("Please enter desired ticket for purchase: ")) 
-        Finished = input("Finished? Y/N :")
-        if Finished.lower() == "y":
-            break
-    
+    NumDays = int(input("Please input the number of days (1/2): "))
+
     AttractionTickets = []
+
+    # Gets inputs from user
     while True:
-        AttractionTickets.append(input("Please enter desired attractions for purchase: "))
-        Finished = input("Finished? Y/N :")
-        if Finished.lower() == "y":
+        Adults = int(input("Please input the number of adults: "))
+        Children = int(input("Please input the number of children: "))
+        Elders = int(input("Please input the number of elders: "))
+        if Children < 2 * Adults:
             break
+        else:
+            print("Not enough adults for children. Please retry.")
+
+    Total = 0
+    SixOrMoreTotal = 0
+    FamilyTotal = 0
+    NormalTotal = 0 
+
+    # One-day prices
+    if NumDays == 1:
+        # Calculate "normal" ticket prices
+        NormalTotal = Adults * TicketTypeOneDay[0] + Children * TicketTypeOneDay[1] + Elders * TicketTypeOneDay[2]
+        print(str(NormalTotal) + " normal")
+
+        # Family of 2 adults/elders and 3 children (perfect family ticket type)
+        if (Adults and Elders > 2) and (Children >= 3) and ((Adults + Elders) % 2 == 0) and (Children % 3 == 0):
+            FamilyTotal = ((Adults + Elders + Children) / 5) * TicketTypeOneDay[3] 
+            print(str(FamilyTotal) + " perfect")
+
+        # Family of >2 adults/elders or >3 children (imperfect family ticket type)  
+        if (((Adults + Elders) % 2) != 0) or (Children % 3 != 0):
+            Zeroer = lambda a: int(abs(a) + a)/2 # Lambda function returns a zero if number is negative
+
+            # Finds the perfect number of tickets through floor division
+            NumberOfPerfectTickets = 0
+            if (Adults + Elders) // 2 < Children // 3:
+                NumberOfPerfectTickets = int((Adults + Elders) // 2)
+            else:
+                NumberOfPerfectTickets = int(Children // 3)
+
+            # Add perfect number of tickets with imperfect/standalone tickets
+            # Zeroer zeroes negative values, eg. if no of elders - perfect tickets < 0, value returned is 0
+            FamilyTotal = (NumberOfPerfectTickets * TicketTypeOneDay[3]) + (Zeroer(Adults - (NumberOfPerfectTickets * 2)) * TicketTypeOneDay[0]) + (Zeroer(Children - (NumberOfPerfectTickets * 3)) * TicketTypeOneDay[1]) + (Zeroer(Elders - (NumberOfPerfectTickets * 2)) * TicketTypeOneDay[2])
+            print(str(FamilyTotal) + " imperfect")
+
+        # Group of six for 1 day
+        if Adults + Elders + Children >= 6:
+            SixOrMoreTotal = SixOrMoreTotal + (TicketTypeOneDay[4] * (Adults + Elders + Children))
+            print(str(SixOrMoreTotal) + " six or more")
+
+        # Codeblock for getting the best prices
+        FamilyTotal
+        SixOrMoreTotal
+        NormalTotal
+
+        if (SixOrMoreTotal < NormalTotal):
+            Total = SixOrMoreTotal
+        if (FamilyTotal < NormalTotal):
+            Total = FamilyTotal
+        if ((SixOrMoreTotal > NormalTotal) and (FamilyTotal > NormalTotal)):
+            Total = NormalTotal
+        if ((SixOrMoreTotal == 0) and (FamilyTotal == 0)):
+            Total = NormalTotal
+
+        print(Total)
+     
+
+    # Function for attraction prices calculator 
+    while True:
+        AttractionsBool = input("Extra attractions? (y/n): ")
+        if AttractionsBool.lower() == "n":
+            break
+
+        # Creates a 2D array of desired tickets for looping and calculating
+        DummyVar = []
+        DummyVar.append(int(input("Please input the desired attraction tickets: ")))
+        DummyVar.append(int(input("Please input the amount wanted: ")))
+        AttractionTickets.append(DummyVar)
+        Finished = str(input("Finished? (y/n): "))
+        if Finished.lower() == "y":
+            break 
+    
+    AttractionTotal = 0
+    # Tallies the total number of wanted attraction tickets
+    if AttractionsBool == "y":
+        for i in range(len(AttractionTickets)):
+            AttractionTotal = AttractionTotal + (AttractionTickets[i][1] * AttractionPrice[AttractionTickets[i][0] - 1])
+    
+    BookingTotal = Total + AttractionTotal
+    UniqueID = randint(000000, 999999)
+    print("Your order ID is %s with a total of $%s." % (UniqueID, BookingTotal))
 
 TicketBooking()
